cc0f78068ad33de137d7ebeecb608e7af064a88e
Drew <drewx2@gmail.com>
2013-02-28 00:01:09 -0800

    base DLX files

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,131 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# Include path
+#TARGET_SPECIFIC_HEADER_PATH := device/htc/msm8960-common/include
+
+BOARD_VENDOR := htc
+
+BOARD_HAVE_HTC_FFC 		:= true
+COMMON_GLOBAL_CFLAGS 		+= -DJB_CAMERA_BLOB -DDISABLE_HW_ID_MATCH_CHECK -DHTCLOG
+COMMON_GLOBAL_CPPFLAGS 		+= -DDISABLE_HW_ID_MATCH_CHECK
+
+#QCOM_TUNNEL_LPA_ENABLED := true
+#QCOM_CSDCLIENT_ENABLED := true
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80600000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlx user_debug=31
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x81a08000
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
+
+# Architecture
+TARGET_GLOBAL_CFLAGS 			+= -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS 			+= -mfpu=neon -mfloat-abi=softfp
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+#BOARD_HAVE_HTC_AUDIO := true
+#BOARD_USES_FLUENCE_INCALL := true
+#BOARD_USES_SEPERATED_AUDIO_INPUT := true
+#TARGET_QCOM_AUDIO_VARIANT := caf
+
+# Bluetooth
+#BOARD_HAVE_BLUETOOTH := true
+#BOARD_HAVE_BLUETOOTH_QCOM := true
+#BLUETOOTH_HCI_USE_MCT := true
+
+# Camera
+#TARGET_PROVIDES_CAMERA_HAL := true
+
+# QCOM GPS
+#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+#TARGET_QCOM_DISPLAY_VARIANT := caf
+#BOARD_HAVE_OLD_ION_API := true
+#BOARD_EGL_NEEDS_LEGACY_FB 		:= true
+BOARD_EGL_CFG 				:= device/htc/msm8960-common/configs/egl.cfg
+TARGET_USES_OVERLAY := true
+TARGET_USES_SF_BYPASS := true
+
+
+# Lights
+#TARGET_PROVIDES_LIBLIGHT := true
+
+# Power
+TARGET_PROVIDES_POWERHAL := true
+
+# RIL
+BOARD_RIL_CLASS := "../../../device/htc/msm8960-common/libril/"
+
+# Time
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_MODULE_NAME := prima_wlan
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+BOARD_USES_SECURE_SERVICES := true
+BOARD_USES_EXTRA_THERMAL_SENSOR := true
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
